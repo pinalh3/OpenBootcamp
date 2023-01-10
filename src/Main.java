209530c959011299648c06bf0bc9d3263c59c855
pinalh3 <pinalh3@gmail.com>
2023-01-10 18:54:19 -0400

    Puertas de coche

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(suma(2,4,6));
+        Coche miCoche= new Coche();
+        miCoche.masPuertas();
+        System.out.println(miCoche.puertas);
+
     }
 
-    public static int suma(int a, int b, int c){
-        var sum= a+b+c;
-        return sum;
+    public static class Coche{
+        int puertas;
+
+        public Coche(){
+
+        }
+
+        public void masPuertas(){
+            puertas++;
+        }
     }
 }
